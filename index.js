779baf0ffd62dfec2c46430b39b32fc1c7cf9b2e
KadinM1 <arcanecartoon50@gmail.com>
2023-02-28 11:07:05 -0800

    build finalize commit
    Fixed package.json not able to launch application

--- index.js
+++ index.js
@@ -25,11 +25,13 @@ function passwordgen(plength, psymbols, plowercase, puppercase, pexcludeSimilarC
 
 
 genrouter.post('/user/:id/:length?/:esc?', function (req, res) {
+    let PROFILEID = req.params.id
     let length = req.params.length
     let excludeSimCharacters = req.params.esc || "uknown"
     let exclude = req.params.esx || "unknown"
     console.log(moment().format() + " password generator used " + (req.params.id || "unkown"))
     res.write(passwordgen(10, false, false, false, true))
+    res.write(PROFILEID)
     res.end()
 })
 
