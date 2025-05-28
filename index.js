1191990b0b7c9a50cfec7235bb636a07533f76b1
kadi1m <61072364+kadi1m@users.noreply.github.com>
2025-05-28 00:03:18 -0500

    added seperate .get with options

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const moment = require('moment')
+const moment = require('moment')
 
 const options = { "caseSensitive": true, "strict": true }
 
@@ -25,23 +25,28 @@ function passwordgen(plength, psymbols, plowercase, puppercase, pexcludeSimilarC
 
 app.get('/', function(req, res)
 {
-    res.redirect('genie/pass')
+    res.status(404).send("Sorry can't find that!")
     // res.send(passwordgen(12, false, false, false, true))
 })
 
 
-// genrouter.post('/user/:id/:length?/:esc?', function (req, res) {
-//     let PROFILEID = req.params.id
-//     let length = req.params.length
-//     let excludeSimCharacters = req.params.esc || "uknown"
-//     let exclude = req.params.esx || "unknown"
-//     res.write(passwordgen(10, false, false, false, true))
-//     res.write(moment().format() + " password generator used " + (req.params.id || "unkown"))
-//     res.end()
-// })
+genrouter.get('/user/:id/length/:length/:symbols/:lowercase/:uppercase', function (req, res) {
+    let PROFILEID = req.params.id || 0
+    let length = req.params.length || 10
+    let symbols = req.params.symbols
+    let lowercase = req.params.lowercase
+    let uppercase = req.params.uppercase
+    // let excludeSimCharacters = req.params.esc || "unknown"
+    // let exclude = req.params.esx || "unknown"
+    res.write(passwordgen(length, symbols, lowercase, uppercase, true))
+    res.write('\n')
+    res.write(moment().format() + " password generator used by userid: " + (req.params.id || "unkown"))
+    res.end()
+})
+//localhost:3000/genie/user/10/length/50/true/true/true
 
 genrouter.get('/pass', function (req, res) {
-    console.log(moment().format() + " password generator used " + (req.params.id || "unkown"))
+    // console.log(moment().format() + " password generator used " + (req.params.id || "unkown"))
     res.write(passwordgen(10, false, false, false, true))
     res.end()
 })
