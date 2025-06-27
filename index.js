4ac3a41a7875d90cbb570790433ed20773db20ec
kadi1m <61072364+kadi1m@users.noreply.github.com>
2025-06-27 16:10:45 -0500

    update from local

--- index.js
+++ index.js
@@ -18,43 +18,58 @@ function passwordgen(plength, psymbols, plowercase, puppercase, pexcludeSimilarC
         lowercase: plowercase,
         uppercae: puppercase,
         pexcludeSimilarCharacter: pexcludeSimilarCharacter,
+        exclude: '/"$`',
         strict: true
     });
     return password
 }
 
-app.get('/', function(req, res)
-{
+app.get('/', function (req, res) {
     res.status(404).send("Sorry can't find that!")
     // res.send(passwordgen(12, false, false, false, true))
 })
 
 
-genrouter.get('/user/:id/length/:length/:symbols/:lowercase/:uppercase', function (req, res) {
-    let PROFILEID = req.params.id || 0
-    let length = req.params.length || 10
-    let symbols = req.params.symbols
-    let lowercase = req.params.lowercase
-    let uppercase = req.params.uppercase
-    // let excludeSimCharacters = req.params.esc || "unknown"
-    // let exclude = req.params.esx || "unknown"
-    res.write(passwordgen(length, symbols, lowercase, uppercase, true))
-    res.write('\n')
-    res.write(moment().format() + " password generator used by userid: " + (req.params.id || "unkown"))
-    res.end()
-})
+// genrouter.get('/user/:id/length/:length/:symbols/:lowercase/:uppercase', function (req, res) {
+//     let PROFILEID = req.params.id || 0
+//     let length = req.params.length || 10
+//     let symbols //req.params.symbols
+//     let lowercase //req.params.lowercase
+//     let uppercase //req.params.uppercase
+//     if (req.params.symbols === "true") {
+//         symbols = true
+//     }else {
+//         symbols = false
+//     }
+//     if (req.params.lowercase === "true") {
+//         lowercase = true
+//     }else {
+//         lowercase = false
+//     }
+//     if (req.params.uppercase === "true") {
+//         uppercase = true
+//     }else {
+//         uppercase = false
+//     }
+//     // let excludeSimCharacters = req.params.esc || "unknown"
+//     // let exclude = req.params.esx || "unknown"
+//     res.write(passwordgen(length, symbols, lowercase, uppercase, true))
+//     res.write('\n')
+//     res.write(moment().format() + " password generator used by userid: " + (req.params.id || "unkown"))
+//     res.end()
+// })
 //localhost:3000/genie/user/10/length/50/true/true/true
 
-genrouter.get('/pass', function (req, res) {
+genrouter.get('/pass/:length', function (req, res) {
+    let length = req.params.length
+    // console.log(length)
+    if(length.length > 4) {
+        length = 512
+    }
+    console.log(length.length)
+    console.log(length)
     // console.log(moment().format() + " password generator used " + (req.params.id || "unkown"))
-    res.write(passwordgen(10, false, false, false, true))
-    res.end()
-})
-
-// console.log(passwordgen(10, false, false, false, true))
-
-genrouter.get('/pass24', function (req, res){
-    res.write(passwordgen(24, true, true, true, false ));
+    res.write(passwordgen(length, true, true, true, true))
     res.end()
 })
 app.listen(process.env.PORT || 3000)
