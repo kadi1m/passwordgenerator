2a92a7db98900a837d2f1979272a7fc5e2ebb888
kadi1m <61072364+kadi1m@users.noreply.github.com>
2025-07-07 23:48:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const moment = require('moment')
+const cors = require('cors');
 
 const options = { "caseSensitive": true, "strict": true }
 
 const genrouter = express.Router([options])
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use('/genie', genrouter)
 
 var generator = require('generate-password');
